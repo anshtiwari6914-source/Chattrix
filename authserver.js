247f647a6d36bda73eb80d25e0e8d7426d687155
Ansh <anshtiwari6914@gmail.com>
2025-12-11 15:21:27 +0530

    add

---
+++ authserver.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 4000;
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, "auth")));
+
+// Default route → index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "auth", "auth.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+const { exec } = require("child_process");
+
+exec("node server.js", (err, stdout, stderr) => {
+    if (err) {
+        console.error("Error:", err);
+        return;
+    }
+    console.log(stdout);
+});
